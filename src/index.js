88109238e0edda8506c76f2750ce288de5376d09
Varvara Gaponova <gaponova.varvara21091995@gmail.com>
2023-01-23 19:57:16 +0400

    feat: finished task

--- src/index.js
+++ src/index.js
@@ -1,44 +1,69 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+    const arr = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        arr.push(expr.slice(i, i + 10));
+    }
+    const newArr = arr.map((item) => {
+        return parseInt(item) || item;
+    });
+
+    const arrS = newArr.map(elem => {
+        const twoElementsArr = [];
+        const item = String(elem);
+        if (item.includes('*')) twoElementsArr.push(' ');
+        else {
+            for (let i = 0; i < item.length; i += 2) {
+                if (+`${item[i]}${item[i + 1]}` === 10) {
+                    twoElementsArr.push('.');
+                } else {
+                    twoElementsArr.push('-');
+                }
+            }
+        }
+        return twoElementsArr.join('');
+    }).map(element => {
+        return MORSE_TABLE[element] || ' ';
+    }).join('');
+    return arrS;
 }
 
 module.exports = {
